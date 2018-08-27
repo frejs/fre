c06da74dc9a5ee79d1bf224e0110b579a547368e
132yse <1533540012@qq.com>
2018-08-27 14:12:34 +0800

    打包并提交新版本

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: './src/fre.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'fre.js',
+    library: "fre",
+    libraryTarget: "umd"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        loader: "ts-loader"
+      }
+    ]
+  }
+
+}
