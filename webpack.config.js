f59b44734d762d8ae6245d089496afcfa1b2bad1
132yse <1533540012@qq.com>
2018-08-27 15:13:06 +0800

    继续修复bug

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   mode: 'production',
-  entry: './src/fre.ts',
+  entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'fre.js',
