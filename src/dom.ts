3d628323463ba70b8caf6c7cda546c6274c1e5e0
132yse <1533540012@qq.com>
2018-08-27 15:11:57 +0800

    修复样式的bug

--- src/dom.ts
+++ src/dom.ts
@@ -1,26 +1,27 @@
-export function setAttr(node:any, name:string, value:string) {
-  if (/on\w+/.test(name)) {
-    name = name.toLowerCase()
-    node[name] = value
-  } else {
-    switch (name) {
-      case 'className':
-        name = 'class'
-        break
-      case 'value':
-        if (node.tagName.toUpperCase() === 'INPUT' || node.tagName.toUpperCase() === 'TEXTAREA') {
-          node.value = value
-        } else {
-          node.setAttribute(name, value)
+export function setAttr(node: any, name: string, value: string) {
+    if (/on\w+/.test(name)) {
+        name = name.toLowerCase()
+        node[name] = value
+    } else {
+        switch (name) {
+            case 'className':
+                name = 'class'
+                node[name] = value
+                break
+            case 'value':
+                if (node.tagName.toUpperCase() === 'INPUT' || node.tagName.toUpperCase() === 'TEXTAREA') {
+                    node.value = value
+                } else {
+                    node.setAttribute(name, value)
+                }
+                break
+            case 'style':
+                node.style.cssText = value
+                break
+            default:
+                node.setAttribute(name, value)
+                break
         }
-        break
-      case 'style':
-        node.style.cssText = value
-        break
-      default:
-        node.setAttribute(name, value)
-        break
     }
-  }
 
 }
