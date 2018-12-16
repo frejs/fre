d283f46fc7f0b53cce3e74f5d6974a52d8300ce9
132yse <1533540012@qq.com>
2018-12-16 22:37:08 +0800

    日常提交

--- dist/fre.e31bb0bc.js
+++ dist/fre.e31bb0bc.js
@@ -633,7 +633,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61466" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64272" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
