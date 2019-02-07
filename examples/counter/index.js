92fcd227ae86ea9dd5f4fd49de9b66b5ecc9dcec
132yse <1533540012@qq.com>
2019-02-07 23:34:15 +0800

    提交提交

--- examples/counter/index.js
+++ examples/counter/index.js
@@ -1,21 +1,10 @@
-import { render, useState, h } from "../../packages/core"
-
-// function Sex() {
-//   const [sex, setSex] = useState("boy")
-//   return (
-//     <div class="sex">
-//       <h1>{sex}</h1>
-//       <button onClick={() => setSex(sex === "boy" ? "girl" : "boy")}>x</button>
-//       <Counter />
-//     </div>
-//   )
-// }
+import { h,render } from "../../packages/core"
 
 function Counter() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
   return (
     <div class="counter">
-      <h1>{count}</h1>
+      <h1>111</h1>
       <button onClick={() => setCount(count + 1)}>+</button>
     </div>
   )
