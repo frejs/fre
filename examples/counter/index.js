a2d7334d5a683bf5e9d44eacdce8bd192993ae8a
132yse <1533540012@qq.com>
2019-02-10 01:17:57 +0800

    彻底搞定hooks

--- examples/counter/index.js
+++ examples/counter/index.js
@@ -1,23 +1,26 @@
-import { h,render,useState } from "../../src"
+import { h, render, useState } from '../../src'
 
 function Counter() {
   const [count, setCount] = useState(0)
+  const [sex, setSex] = useState('boy')
   return (
     <div class="counter">
       <h1>{count}</h1>
       <button onClick={() => setCount(count + 1)}>+</button>
-      <Sex />
+      <h1>{sex}</h1>
+      <button onClick={() => {sex === 'boy' ? setSex('girl') : setSex('boy')}}>x</button>
     </div>
   )
 }
 
-function Sex(){
-  const [sex, setSex] = useState('boy')
-  return (
-    <div class="counter">
-      <h1>{sex}</h1>
-    </div>
-  )
-}
+// function Sex() {
+//   const [sex, setSex] = useState('boy')
+//   return (
+//     <div>
+//       <h1>{sex}</h1>
+//       <button onClick={()=>{sex === 'boy' ? setSex('girl') : setSex('boy')}}>x</button>
+//     </div>
+//   )
+// }
 
 render(<Counter />, document.getElementById('app'))
