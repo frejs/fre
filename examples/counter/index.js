d0960b0bdf1efccb6235d4b455ae0e2da152d138
132yse <1533540012@qq.com>
2019-02-10 02:40:47 +0800

    彻底彻底搞定了呜呜呜

--- examples/counter/index.js
+++ examples/counter/index.js
@@ -9,10 +9,24 @@ function Counter() {
       <button onClick={() => setUp(up + 1)}>+</button>
       <h1>{down}</h1>
       <button onClick={() => setDown(down -1)}>-</button>
-      {/* <Sex /> */}
+      <Count />
     </div>
   )
 }
+
+function Count() {
+  const [add, setAdd] = useState(0)
+  const [cut, setCut] = useState(0)
+  return (
+    <div class="counter">
+      <h1>{add}</h1>
+      <button onClick={() => setAdd(add + 1)}>+</button>
+      <h1>{cut}</h1>
+      <button onClick={() => setCut(cut -1)}>-</button>
+    </div>
+  )
+}
+
 
 // function Sex() {
 //   const [sex, setSex] = useState('boy')
