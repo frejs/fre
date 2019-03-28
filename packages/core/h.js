d7876209a0f2e47f9185033c12ff8693a74b5785
132yse <1533540012@qq.com>
2019-03-28 09:18:30 +0800

    重构h函数

---
+++ packages/core/h.js
@@ -0,0 +1,28 @@
+export function h (type, props) {
+  let rest = []
+  let children = []
+  let length = arguments.length
+
+  while (length-- > 2) rest.push(arguments[length])
+
+  while (rest.length) {
+    let node = rest.pop()
+    if (node && node.pop) {
+      for (length = node.length; length--;) {
+        rest.push(node[length])
+      }
+    } else if (node === null || node === true || node === false) {
+    } else {
+      children.push(node)
+    }
+  }
+
+  return typeof type === 'function'
+    ? name(props || {}, children)
+    : {
+      type,
+      props: props || {},
+      children,
+      key: props && props.key
+    }
+}
