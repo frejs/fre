67a8cd33be723f3b6d96054dc245466ab3001f96
132yse <1533540012@qq.com>
2019-05-11 16:40:31 +0800

    日常提交

--- examples/doc/index.js
+++ examples/doc/index.js
@@ -8,6 +8,17 @@ function App () {
   })
   return (
     <div class='container'>
+      <div class='logo'>
+        <p align='center'>
+          <img
+            src='http://wx2.sinaimg.cn/mw690/0060lm7Tly1ftpm5b3ihfj3096097aaj.jpg'
+            alt='fre logo'
+            width='180'
+          />
+        </p>
+        <h1 align='center'>Fre</h1>
+        <h3 align='center'>Fast 1kb React-like hooks API js library</h3>
+      </div>
       <div class='wrap' />
     </div>
   )
