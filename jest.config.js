f2cae69ab4a62e894aba4f58feb7540571c9fb82
伊撒尔 <1533540012@qq.com>
2019-07-11 15:12:32 +0800

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  'transform': {
-    '^.+\\.(jsx?|tsx?)$': 'babel-jest'
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-}
