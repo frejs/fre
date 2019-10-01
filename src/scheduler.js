0b499bdc76db033a19bed80bd6f9e4de6612c056
132yse <1533540012@qq.com>
2019-10-01 14:16:55 +0800

    finished scheduler prototype

--- src/scheduler.js
+++ src/scheduler.js
@@ -1,5 +1,15 @@
 let firstTask = null
+let currentTask = null
 let isPerform = false
+let isSchedule = false
+let FD = 0
+let FPS = 20
+let timeoutTime = -1
+let inMacro = false
+let deadline = {
+  didTimeout: false,
+  timeRemaining: FD - performance.now()
+}
 
 function schedulerCallback (cb, dueTime, config) {
   let currentTime = performance.now()
@@ -23,12 +33,66 @@ function schedulerCallback (cb, dueTime, config) {
   if (startTime > currentTime) {
   } else {
     insertTask(newTask, dueTime)
-    if (!isPerform) {
-      requestHostCallback(flushWork)
+    if (!isPerform) requestHostCallback(flushWork, dueTime)
+  }
+  return newTask
+}
+
+function flushWork (didTimeout) {
+  deadline.didTimeout = didTimeout
+  try {
+    if (didTimeout) {
+      while (firstTask != null) {
+        let currenTime = performance.now()
+        if (firstTask.dueTime <= currenTime) {
+          do {
+            flushTask()
+          } while (firstTask != null && firstTask.dueTime <= currenTime)
+          continue
+        }
+        break
+      }
+    } else {
+      if (firstTask != null) {
+        do {
+          flushTask()
+        } while (firstTask != null && FD - performance.now() > 0)
+      }
+    }
+  } finally {
+    if (firstTask != null) {
+      requestHostCallback(flushTask, firstTask.dueTime)
+    } else {
+      isPerform = false
     }
   }
+}
 
-  return newTask
+function flushTask () {
+  let flushed = firstTask
+  let next = firstTask.next
+  if (firstTask === next) {
+    firstTask = null
+    next = null
+  } else {
+    let prev = firstTask.prev
+    firstTask = prev.next = next
+    next.prev = prev
+  }
+  flushed.next = flushed.prev = null
+
+  let cb = flushed.cb
+  let dueTime = flushed.dueTime
+  let prevLevel = currentLevel
+  let prevDueTime = currentDueTime
+  currentDueTime = dueTime
+
+  try {
+    cb(deadline)
+  } finally {
+    currentLevel = prevLevel
+    currentDueTime = prevDueTime
+  }
 }
 
 function insertTask (newTask, dueTime) {
@@ -45,7 +109,6 @@ function insertTask (newTask, dueTime) {
       }
       task = task.next
     } while (task != firstTask)
-
     if (next == null) {
       next = task
     } else if (next == firstTask) {
@@ -56,5 +119,72 @@ function insertTask (newTask, dueTime) {
     prev.next = next.prev = newTask
     newTask.next = next
     newTask.prev = prev
+  }
+}
+
+function requestHostCallback (cb, dt) {
+  if (isPerform) {
+    cancelHostCallback()
+  }
+  currentTask = cb
+  if (dt < 0) {
+    setTimeout(idle)
+  } else if (!isSchedule) {
+    defer(tick)
+  }
+}
+
+function cancelHostCallback () {
+  isSchedule = null
+  timeoutTime = -1
+}
+
+function defer (cb) {
+  rafid = requestAnimationFrame(time => {
+    clearTimeout(outid)
+    cb(time)
+  })
+  outid = setTimeout(() => {
+    cancelAnimationFrame(rafid)
+    cb(new Date())
+  }, 100)
+}
+
+function tick (rafTime) {
+  isSchedule ? defer(tick) : (isPerform = false)
+  let nextTime = rafTime - FD + FPS
+  if (nextTime < FPS && prevTime < FPS) {
+    if (nextTime < 8) nextTime = 8
+    FPS = nextTime < prevTime ? prevTime : nextTime
+  } else prevTime = nextTime
+
+  FD = rafTime + FPS
+  if (!isEvent) setTimeout(idle)
+}
+
+function idle (rafTime) {
+  inMacro = true
+  let currentTime = performance.now()
+  let didTimeout = false
+  let scheduleCb = null
+  let prevCb = scheduleCb
+
+  if (FD - currentTime < 1) {
+    if (prevTime < currentTime) {
+      didTimeout = true
+    } else {
+      defer(tick)
+      scheduleCb = prevCb
+      return
+    }
+  }
+
+  if (prevCb != null) {
+    isSchedule = true
+    try {
+      prevCb(didTimeout)
+    } finally {
+      isSchedule = false
+    }
   }
 }
