4cd9f44dbfdb57f67954ed44bb7547d241fb12ed
Rasmus Schultz <rasmus@mindplay.dk>
2019-10-06 17:31:57 +0200

    preliminary work on a test for key-based element reordering

--- test/render.test.jsx
+++ test/render.test.jsx
@@ -85,3 +85,66 @@ test('update components; use state and effect hooks', done => {
     }
   })
 })
+
+test('reorder elements using key-based diff', done => {
+  const initialState = [1,2,3]
+
+  const states = [
+    [3,1,2], // shift right
+    [2,3,1], // shift left
+    [1,3],   // remove from middle
+    [2,3],   // remove first
+    [1,2],   // remove last
+    [3,2,1], // reverse order
+  ]
+
+  let update
+
+  const Component = ({initialState}) => {
+    const [state, setState] = useState(initialState)
+
+    const afterEffect = new Promise(resolve => useEffect(resolve))
+
+    update = newState => {
+      setState(newState)
+
+      return afterEffect
+    }
+
+    return (
+      <ul>
+        {state.map(value => <li key={value}>{value}</li>)}
+      </ul>
+    )
+  }
+
+  let promise = testRender(<Component initialState={initialState}/>)
+
+  for (const state of states) {
+    promise = promise.then(() => {
+      return update(state).then(() => {
+        const ul = document.body.children[0]
+
+        // TODO preserve and check element references for identity between updates
+
+        console.log("target state: ", state, [...ul.children].map(child => child.textContent))
+
+        expect(ul.children.length).toBe(state.length)
+
+        state.map((value, index) => {
+          expect(ul.children[index].textContent).toBe("" + value)
+        })
+
+        return update(initialState).then(() => {
+          const ul = document.body.children[0]
+
+          expect(ul.children[0].textContent).toBe("" + initialState[0])
+          expect(ul.children[1].textContent).toBe("" + initialState[1])
+          expect(ul.children[2].textContent).toBe("" + initialState[2])
+        })
+      })
+    })
+  }
+
+  promise.then(done)
+})
