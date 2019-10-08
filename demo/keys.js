312c8f1f509939951016e8e20e5d6a19bab653a9
132 <MP_FED@MP-FEDs-iMac.local>
2019-10-08 14:04:12 +0800

    clean up code

--- demo/keys.js
+++ demo/keys.js
@@ -1,120 +1,59 @@
 import { h, render, useState, useEffect } from '../src'
 
-// A B C D -> B A D C √
+function Counter ({ id, remove }) {
+  const [count, setCount] = useState(0)
 
+  useEffect(() => {
+    document.title = `You clicked ${count} times`
 
+    return () => {
+      document.title = `Counter removed`
+    }
+  })
 
-// function App () {
-//   const [arr, setArr] = useState(1)
-//   return (
-//     <div>
-//       {new Array(arr).fill().map(i=> (
-//         <A key={i}/>
-//       ))}
-//       <button onClick={() => setArr(arr + 1)}>+</button>
-//       <button onClick={() => setArr(arr - 1)}>-</button>
-//     </div>
-//   )
-// }
+  return (
+    <div>
+      Counter {id} is {count}
+      <button onClick={() => setCount(count + 1)}>➕</button>
+      <button onClick={() => setCount(count - 1)}>➖</button>
+      <button onClick={remove}>❌</button>
+    </div>
+  )
+}
 
-// function A (props) {
-//   return <li>1</li>
-// }
+let nextId = 3
 
-// function App () {
-//   const [arr, setArr] = useState(['A', 'B', 'C', 'D'])
-//   return (
-//     <div>
-//       {arr.map(item => (
-//         <li>{item}</li>
-//       ))}
-//       <button onClick={() => setArr(['C', 'D', 'A', 'B'])}>+</button>
-//     </div>
-//   )
-// }
+function App () {
+  const [counters, setCounters] = useState([1, 2, 3])
 
-// function App () {
-//   const [arr, setArr] = useState(['A', 'B', 'C', 'D'])
-//   return (
-//     <div>
-//       {arr.map(item => (
-//         <li key={item}>{item}</li>
-//       ))}
-//       <button onClick={() => setArr(['B', 'A', 'D', 'C'])}>+</button>
-//     </div>
-//   )
-// }
+  console.log(counters)
 
-// A B C D -> C D A B √
+  return (
+    <div>
+      {counters.map(id => (
+        <Counter
+          id={id}
+          remove={() => setCounters(counters.filter(c => c !== id))}
+        />
+      ))}
+      <hr />
+      <button onClick={() => setCounters(counters.concat(++nextId))}>
+        Add new
+      </button>
+      <button
+        onClick={() => setCounters(counters.slice(0, counters.length - 1))}
+        disabled={counters.length === 1}
+      >
+        Remove last
+      </button>
+      <button
+        onClick={() => setCounters(counters.slice().reverse())}
+        disabled={counters.length === 1}
+      >
+        Reverse
+      </button>
+    </div>
+  )
+}
 
-// function App () {
-//   const [arr, setArr] = useState(['A','B','C','D'])
-//   return (
-//     <div>
-//       <ul>
-//         {arr.map(item => (
-//           <li key={item}>{item}</li>
-//         ))}
-//       </ul>
-//       <button onClick={() => setArr(['B','C','D','A','E'])}>+</button>
-//     </div>
-//   )
-// }
-
-// function App () {
-//   const [arr, setArr] = useState(['A','B','C','D'])
-//   return (
-//     <div>
-//       <ul>
-//         {arr.map(item => (
-//           <li key={item}>{item}</li>
-//         ))}
-//       </ul>
-//       <button onClick={() => setArr(['B','C','A','D'])}>+</button>
-//     </div>
-//   )
-// }
-
-
-// function App () {
-//   const [arr, setArr] = useState(['A', 'B'])
-//   return (
-//     <div>
-//         {arr.map(item => (
-//           <li key={item}>{item}</li>
-//         ))}
-//       <button onClick={() => setArr(['B', 'A'])}>+</button>
-//     </div>
-//   )
-// }
-
-// function App () {
-//   const [arr, setArr] = useState(['A', 'B'])
-//   return (
-//     <div>
-//       <ul>
-//         {arr.map(item => (
-//           <li key={item}>{item}</li>
-//         ))}
-//       </ul>
-//       <button onClick={() => setArr(['B'])}>+</button>
-//     </div>
-//   )
-// }
-
-// function App () {
-//   const [arr, setArr] = useState(['A', 'B', 'C', 'D', 'E', 'F'])
-//   return (
-//     <div>
-//       <ul>
-//         {arr.map(item => (
-//           <li key={item}>{item}</li>
-//         ))}
-//       </ul>
-//       <button onClick={() => setArr(['E', 'F', 'C', 'D', 'A', 'B'])}>+</button>
-//     </div>
-//   )
-// }
-
-
-render(<App />, document.getElementById('root'))
+render(<App />, document.body)
