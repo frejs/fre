7d9e6d801674b0f8e889827726c3ec778ba63c84
132 <MP_FED@MP-FEDs-iMac.local>
2019-10-09 19:16:16 +0800

    clean up code

--- src/hooks.js
+++ src/hooks.js
@@ -5,7 +5,6 @@ function update (key, reducer, value) {
   const current = this ? this : getWIP()
   value = reducer ? reducer(current.state[key], value) : value
   current.state[key] = value
-  console.log(current.key)
   scheduleWork(current, true)
 }
 export function resetCursor () {
