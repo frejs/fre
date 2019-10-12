7d77fcfa8964ae72265c593a8aa2f19ee883769f
Rasmus Schultz <rasmus@mindplay.dk>
2019-10-12 17:43:59 +0200

    simplify style-property updates and add tests

--- test/render.test.jsx
+++ test/render.test.jsx
@@ -60,6 +60,7 @@ test('render range of HTML elements', async () => {
 
   expect(toString(elements)).toBe("<ul><li>1</li><li>2</li><li>3</li></ul>")
 })
+
 test('render/update object properties and DOM attributes', async () => {
   let lastChildren = []
 
@@ -235,4 +236,30 @@ test('reorder and reuse elements during key-based reconciliation of child-nodes'
       lastChildren = children
     }
   })))
+})
+
+test('diff style-object properties', async () => {
+  await testUpdates([
+    {
+      content: <div style={{color: "red", backgroundColor: "blue"}}/>,
+      test: ([div]) => {
+        expect(div.style.color).toBe("red")
+        expect(div.style.backgroundColor).toBe("blue")
+      }
+    },
+    {
+      content: <div style={{color: "yellow", fontSize: "99px"}}/>,
+      test: ([div]) => {
+        expect(div.style.color).toBe("yellow")
+        expect(div.style.backgroundColor).toBe("")
+        expect(div.style.fontSize).toBe("99px")
+      }
+    },
+    {
+      content: <div/>,
+      test: ([div]) => {
+        expect(div.style.color).toBe("")
+      }
+    },
+  ])
 })
