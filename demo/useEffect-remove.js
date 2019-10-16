8cb3bc932b850647e791cc1e0ae39d56439c6ae7
132yse <1533540012@qq.com>
2019-10-16 20:56:35 +0800

    add example

--- demo/useEffect-remove.js
+++ demo/useEffect-remove.js
@@ -1,11 +1,12 @@
-// import { h, render, useState, useEffect } from '../src'
-import { render } from 'react-dom'
-import { createElement as h, useState, useEffect, useRef } from 'react'
+import { h, render, useState, useEffect } from '../src'
+// import { render } from 'react-dom'
+// import { createElement as h, useState, useEffect, useRef } from 'react'
 
 function App () {
   let effects = []
   let effectNum = 1
   const [state, setState] = useState(true)
+  const [count, setCount] = useState(0)
   useEffect(() => {
     const currentEffectNum = effectNum++
 
@@ -14,14 +15,20 @@ function App () {
 
     return () => {
       effects.push(`cleanUp ${currentEffectNum}`)
+      console.log(effects)
     }
   })
   return (
     <div>
-      {state && <div>111</div>}
+      {state && <A val={count} />}
       <button onClick={() => setState(false)}>remove</button>
+      <button onClick={() => setCount(count + 1)}>add</button>
     </div>
   )
+}
+
+function A ({ val }) {
+  return <div>{val}</div>
 }
 
 render(<App />, document.body)
