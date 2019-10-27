441709d93415e2ac77c0569ac68cd24b4f160573
132yse <1533540012@qq.com>
2019-10-27 13:18:13 +0800

    clean up code

--- demo/suspense/index.js
+++ demo/suspense/index.js
@@ -15,12 +15,11 @@ const result = useSuspense(fetchUsers)
 function App () {
   const [state, setState] = useState(1)
   const users = result.read(state)
-  const handler = () => setState(state + 1)
 
   return (
     <main>
       <h1>Fetching clicli users</h1>
-      <button onClick={handler}>Click {state}</button>
+      <button onClick={() => setState(state + 1)}>Click {state}</button>
       {users.map(user => (
         <img alt='woof' src={`https://q1.qlogo.cn/g?b=qq&nk=${user.qq}&s=640`} />
       ))}
