c058e668850eabad5c78e2a209d200eb43b474cf
Rasmus Schultz <rasmus@mindplay.dk>
2019-10-27 14:55:47 +0100

    add test for functional state updates

--- test/render.test.jsx
+++ test/render.test.jsx
@@ -368,3 +368,39 @@ test('diff style-object properties', async () => {
     }
   ])
 })
+
+test('async state update', async () => {
+  let updates = 0
+
+  const Component = () => {
+    const [count, setState] = useState(0)
+
+    updates += 1
+
+    return <button onClick={() => setState(count => count + 1)}>{count}</button>
+  }
+
+  const content = <Component/>
+
+  await testUpdates([
+    {
+      content,
+      test: ([button]) => {
+        expect(+button.textContent).toBe(0)
+        expect(updates).toBe(1)
+
+        // trigger several functional state updates:
+        button.click()
+        button.click()
+        button.click()
+      }
+    },
+    {
+      content,
+      test: ([button]) => {
+        expect(+button.textContent).toBe(3) // all 3 state updates applied
+        expect(updates).toBe(2)             // but component only renders once
+      }
+    }
+  ])
+})
