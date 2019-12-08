52e31d633bd976f800e66357ffd46259b4cd1469
132yse <1533540012@qq.com>
2019-12-08 13:22:28 +0800

    add demo for memo

---
+++ demo/memo.js
@@ -0,0 +1,20 @@
+import { h, render, useState } from '../src'
+
+function App () {
+  const [state, setState] = useState(0)
+  return (
+    <div>
+      {state}
+      <A count={1}/>
+      <button onClick={() => setState(state+1)}>+</button>
+    </div>
+  )
+}
+
+function A(props){
+  console.log(222)
+  return <div>{props.count}</div>
+}
+
+
+render(<App />, document.getElementById('root'))
