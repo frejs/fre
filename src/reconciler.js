c9df2b221e9ee8c3effae1b9789104a1b9b86680
132yse <1533540012@qq.com>
2019-12-09 15:37:03 +0800

    fix lock bug

--- src/reconciler.js
+++ src/reconciler.js
@@ -78,7 +78,7 @@ function reconcile(WIP) {
 function updateHOOK(WIP) {
   const oldProps = WIP.pendingProps
   const newProps = WIP.props
-  if (WIP.lock === null && !shouldUpdate(oldProps, newProps)) {
+  if (WIP.lock === false && !shouldUpdate(oldProps, newProps)) {
     cloneChildren(WIP)
     return
   }
