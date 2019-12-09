645d12241dec6410ad27261b26eaa61dafad1d48
132yse <1533540012@qq.com>
2019-12-09 21:30:38 +0800

    clean up code

--- src/reconciler.js
+++ src/reconciler.js
@@ -183,9 +183,7 @@ function cloneChildren(fiber) {
 
 function shouldUpdate(a, b) {
   for (let i in a) if (!(i in b)) return true
-  for (let i in b) {
-    if (a[i] !== b[i]) return true
-  }
+  for (let i in b) if (a[i] !== b[i]) return true
   return false
 }
 
