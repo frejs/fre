7e92e6cd8efc2d4e94e553eb9989c3ac352220b0
木杉 <zhmushan@qq.com>
2020-01-04 17:01:03 +0800

    Replace arguments with ...args

--- src/h.js
+++ src/h.js
@@ -1,16 +1,25 @@
-export function h(type, attrs) {
+const hasOwnProperty = Object.prototype.hasOwnProperty
+
+export function h(type, attrs, ...args) {
   let props = attrs || {}
   let key = props.key || null
   let ref = props.ref || null
   let children = []
 
-  for (let i = 2; i < arguments.length; i++) {
-    let vnode = arguments[i]
-    if (vnode == null || vnode === true || vnode === false) {
-    } else if (typeof vnode === 'string' || typeof vnode === 'number') {
-      children.push(createText(vnode))
-    } else {
-      children.push(vnode)
+  for (const key in args) {
+    if (hasOwnProperty.call(args, key)) {
+      const child = args[key]
+      const type = typeof child
+
+      if (type === 'boolean' || child == null) {
+        break
+      }
+
+      if (type === 'string' || type === 'number') {
+        args[key] = createText(args[key])
+      }
+
+      children.push(args[key])
     }
   }
 
