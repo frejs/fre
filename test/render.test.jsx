3f1fd0292a58d3d0776c7eddc78cc076568eb376
ahaoboy <504595380@qq.com>
2020-03-06 15:37:12 +0800

    add test: render 3D array

--- test/render.test.jsx
+++ test/render.test.jsx
@@ -31,3 +31,29 @@ test('render range of HTML elements', async () => {
 
   expect(toString(elements)).toBe('<ul><li>1</li><li>2</li><li>3</li></ul>')
 })
+
+test('render 3D array', async () => {
+  const elements = await testRender(
+    <ul>
+      {Array(2)
+        .fill()
+        .map((_, i) =>
+          Array(2)
+            .fill()
+            .map((_, j) =>
+              Array(2)
+                .fill()
+                .map((_, k) => (
+                  <li>
+                    {i},{j},{k}
+                  </li>
+                ))
+            )
+        )}
+    </ul>
+  )
+
+  expect(toString(elements)).toBe(
+    '<ul><li>0,0,0</li><li>0,0,1</li><li>0,1,0</li><li>0,1,1</li><li>1,0,0</li><li>1,0,1</li><li>1,1,0</li><li>1,1,1</li></ul>'
+  )
+})
