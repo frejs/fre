41b9f8abc24fce011a9cc55f4a1e6fade29228a6
132yse <1533540012@qq.com>
2020-03-17 11:53:36 +0800

    implement context API

--- src/context.js
+++ src/context.js
@@ -9,6 +9,7 @@ export function useContext(context) {
   const provider = current.context[context.id]
   if (!provider) return context.defaultValue
   if (!hook[0]) {
+    hook[0] = true
     provider.sub(current)
   }
   return provider.props.value
@@ -23,16 +24,14 @@ export function createContext(defaultValue) {
     },
     Provider(props) {
       let [hook, current] = getHook(cursor)
-      if (!hook.subs) hook[1] = []
+      if (!hook[1]) hook[1] = []
       if (props.value !== hook[0]) {
-        hook[1].forEach(c => {
-          c.context = value
-          scheduleWork(c)
-        })
+        hook[0] = props.value
+        hook[1].forEach(c => scheduleWork(c))
       }
       if (!current.context) {
         current.context = {}
-        current.context[id] = current
+        current.context[id++] = current
         current.sub = c => hook[1].push(c)
       }
       return props.children
