cf0238971f40b07ee8f69d137fd14d3e8d52da5d
132yse <1533540012@qq.com>
2020-05-10 20:55:01 +0800

    rename

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import { h, Fragment, memo } from './h'
+import { render, scheduleWork } from './reconciler'
+import {
+  useState,
+  useReducer,
+  useEffect,
+  useMemo,
+  useCallback,
+  useRef,
+  useLayout,
+} from './hooks'
+
+export {
+  h,
+  h as createElement,
+  Fragment,
+  render,
+  scheduleWork,
+  useState,
+  useReducer,
+  useEffect,
+  useMemo,
+  useCallback,
+  useRef,
+  useLayout,
+  useLayout as useLayoutEffect,
+  memo
+}
+
+const Fre = {
+  h,
+  Fragment,
+  render,
+  scheduleWork,
+  useState,
+  useReducer,
+  useEffect,
+  useMemo,
+  useCallback,
+  useRef,
+  memo
+}
+
+export default Fre
