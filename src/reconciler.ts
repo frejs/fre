f9a109e8b0a2e818afe0c56513aab46ec74e8368
132yse <1533540012@qq.com>
2020-05-19 22:52:49 +0800

    fix bug

--- src/reconciler.ts
+++ src/reconciler.ts
@@ -260,7 +260,7 @@ export const getCurrentFiber = () => currentFiber || null
 export const isFn = (x: any): x is Function => typeof x === 'function'
 export const isStr = (s: any): s is number | string =>
   typeof s === 'number' || typeof s === 'string'
-export const some = (v: any) => v !== null && v !== false && v !== true
+export const some = (v: any) => v != null && v !== false && v !== true
 
 const hs = (i: number, j: number, k: unknown) =>
   k != null && j != null
