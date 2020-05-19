8c9fbada9ce7483d15401386ddcfb9b4dfaf78a1
132yse <1533540012@qq.com>
2020-05-19 20:17:12 +0800

    add lazy

--- src/suspense.ts
+++ src/suspense.ts
@@ -1,7 +1,24 @@
-import { Flag } from './reconciler'
-import { Component } from './type'
+import { Component, Props } from './type'
+import { jsx } from './jsx'
 
-function Lazy(fn: Component) {
-  fn.tag = Flag.LAZY
-  return fn
+export function lazy(loader: Component) {
+  let p
+  let component
+  let error
+
+  function Lazy(props: Props) {
+    if (!p) {
+      p = loader()
+      p.then(
+        exports => (component = exports.default || exports),
+        e => (error = e)
+      )
+    }
+
+    if (error) throw error
+    if (!component) throw p
+
+    return jsx(component, props)
+  }
+  return Lazy
 }
