0b062a42f1a4f79515f70766c0d0a50210265efb
132yse <1533540012@qq.com>
2020-06-02 13:51:25 +0800

    refactor rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,34 @@
 import { terser } from 'rollup-plugin-terser'
 import typescript from 'rollup-plugin-typescript2'
+import dts from 'rollup-plugin-dts'
 
 export const plugins = [
   typescript({
     tsconfig: 'tsconfig.json',
-    removeComments: true
+    removeComments: true,
   }),
   terser({
-    include: ['fre.js']
-  })
+    include: ['fre.js'],
+  }),
 ]
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    { file: 'dist/fre.js', format: 'umd', name: 'fre', sourcemap: true },
-    { file: 'dist/fre.esm.js', format: 'esm', sourcemap: true }
-  ],
-  plugins
-}
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      { file: 'dist/fre.js', format: 'umd', name: 'fre', sourcemap: true },
+      { file: 'dist/fre.esm.js', format: 'esm', sourcemap: true },
+    ],
+    plugins,
+  },
+  {
+    input: 'compat/index.js',
+    output: {
+      file: 'dist/fre-compat.js',
+      format: 'umd',
+      name: 'fre',
+      sourcemap: true,
+    },
+    plugins,
+  },
+]
