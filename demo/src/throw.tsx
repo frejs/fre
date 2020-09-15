b131efa27158877ce0300842ae2e253a1c2dc40d
132yse <1533540012@qq.com>
2020-09-15 20:42:42 +0800

    catch promis

--- demo/src/throw.tsx
+++ demo/src/throw.tsx
@@ -3,21 +3,19 @@ import { h, render, useState, useEffect } from '../../src/index'
 const App = () => {
   const [resource, setResource] = useState(undefined)
   const [count, setCount] = useState(0)
-  const [pending, setPending] = useState(false)
-  //   useEffect(() => {
-  //     const id = setInterval(() => setCount((c) => c + 1), 200)
-  //     return () => clearInterval(id)
-  //   }, [])
+    useEffect(() => {
+      const id = setInterval(() => setCount((c) => c + 1), 1000)
+      return () => clearInterval(id)
+    }, [])
   const update = () => {
-    setPending(true)
     setResource(wrapPromise(new Promise((r) => setTimeout(r, 3000)).then(() => 'FETCHED RESULT')))
   }
 
-  console.log(resource)
+  // console.log(resource,count)
   return (
     <div>
       <button onClick={update}>CLICK ME</button>
-      <pre>{JSON.stringify({ count, pending }, null, 2)}</pre>
+      <pre>{JSON.stringify({ count }, null, 2)}</pre>
       {resource ? resource.read() : 'Initial state'}
     </div>
   )
--- src/reconciler.ts
+++ src/reconciler.ts
@@ -1,24 +1,12 @@
-import {
-  IFiber,
-  FreElement,
-  ITaskCallback,
-  FC,
-  Attributes,
-  HTMLElementEx,
-  FreNode,
-  FiberMap,
-  IRef,
-  IEffect,
-  Option
-} from './type'
+import { IFiber, FreElement, ITaskCallback, FC, Attributes, HTMLElementEx, FreNode, FiberMap, IRef, IEffect, Option } from './type'
 import { createElement, updateElement } from './dom'
-import { resetCursor } from './hooks'
+import { resetCursor, currentHook } from './hooks'
 import { scheduleCallback, shouldYeild, planWork } from './scheduler'
 import { isArr, createText } from './h'
 export const options: Option = {
   catchError(_, e) {
     throw e
-  }
+  },
 }
 
 let preCommit: IFiber | undefined
@@ -27,15 +15,11 @@ let WIP: IFiber | undefined
 let updateQueue: IFiber[] = []
 let commitQueue: IFiber[] = []
 
-export const render = (
-  vnode: FreElement,
-  node: Element | Document | DocumentFragment | Comment,
-  done?: () => void
-): void => {
+export const render = (vnode: FreElement, node: Element | Document | DocumentFragment | Comment, done?: () => void): void => {
   let rootFiber = {
     node,
     props: { children: vnode },
-    done
+    done,
   } as IFiber
   scheduleWork(rootFiber)
 }
@@ -62,28 +46,28 @@ const reconcileWork = (timeout: boolean): boolean | null | ITaskCallback => {
 
 const reconcile = (WIP: IFiber): IFiber | undefined => {
   WIP.parentNode = getParentNode(WIP) as HTMLElementEx
-  if (isFn(WIP.type)) {
-    try {
-      updateHook(WIP)
-    } catch (e) {
-      options.catchError(WIP, e)
+  try {
+    isFn(WIP.type) ? updateHook(WIP) : updateHost(WIP)
+  } catch (e) {
+    if (!!e && typeof e.then === 'function') {
+      console.log(currentHook)
+      return
     }
-  } else {
-    updateHost(WIP)
-  }
-  WIP.dirty = WIP.dirty ? false : 0
-  commitQueue.push(WIP)
+  } finally {
+    WIP.dirty = WIP.dirty ? false : 0
+    commitQueue.push(WIP)
 
-  if (WIP.child) return WIP.child
-  while (WIP) {
-    if (!preCommit && WIP.dirty === false) {
-      preCommit = WIP
-      return null
+    if (WIP.child) return WIP.child
+    while (WIP) {
+      if (!preCommit && WIP.dirty === false) {
+        preCommit = WIP
+        return null
+      }
+      if (WIP.sibling) {
+        return WIP.sibling
+      }
+      WIP = WIP.parent
     }
-    if (WIP.sibling) {
-      return WIP.sibling
-    }
-    WIP = WIP.parent
   }
 }
 
@@ -173,7 +157,7 @@ const shouldPlace = (fiber: IFiber): string | boolean | undefined => {
 }
 
 const commitWork = (fiber: IFiber): void => {
-  commitQueue.forEach(c => c.parent && commit(c))
+  commitQueue.forEach((c) => c.parent && commit(c))
   fiber.done && fiber.done()
   commitQueue.length = 0
   preCommit = null
@@ -207,18 +191,13 @@ const commit = (fiber: IFiber): void => {
 const hashfy = <P>(c: IFiber<P>): FiberMap<P> => {
   const out: FiberMap<P> = {}
   isArr(c)
-    ? c.forEach((v, i) =>
-        isArr(v)
-          ? v.forEach((vi, j) => (out[hs(i, j, vi.key)] = vi))
-          : some(v) && (out[hs(i, null, v.key)] = v)
-      )
+    ? c.forEach((v, i) => (isArr(v) ? v.forEach((vi, j) => (out[hs(i, j, vi.key)] = vi)) : some(v) && (out[hs(i, null, v.key)] = v)))
     : some(c) && (out[hs(0, null, (c as any).key)] = c)
   return out
 }
 
 const refer = (ref: IRef, dom?: HTMLElement): void => {
-  if (ref)
-    isFn(ref) ? ref(dom) : ((ref as { current?: HTMLElement })!.current = dom)
+  if (ref) isFn(ref) ? ref(dom) : ((ref as { current?: HTMLElement })!.current = dom)
 }
 
 const cleanupRef = <P = Attributes>(kids: FiberMap<P>): void => {
@@ -237,27 +216,20 @@ const side = (effects: IEffect[]): void => {
 
 export const getCurrentFiber = () => currentFiber || null
 
-const effect = (e: IEffect): void => e[2] = e[0](currentFiber)
+const effect = (e: IEffect): void => (e[2] = e[0](currentFiber))
 const cleanup = (e: IEffect): void => e[2] && e[2](currentFiber)
 
 export const isFn = (x: any): x is Function => typeof x === 'function'
-export const isStr = (s: any): s is number | string =>
-  typeof s === 'number' || typeof s === 'string'
+export const isStr = (s: any): s is number | string => typeof s === 'number' || typeof s === 'string'
 export const some = (v: any) => v != null && v !== false && v !== true
 
 const hs = (i: number, j: string | number | null, k?: string): string =>
-  k != null && j != null
-    ? '.' + i + '.' + k
-    : j != null
-    ? '.' + i + '.' + j
-    : k != null
-    ? '.' + k
-    : '.' + i
+  k != null && j != null ? '.' + i + '.' + k : j != null ? '.' + i + '.' + j : k != null ? '.' + k : '.' + i
 
 export const enum Flag {
   NOWORK = 0,
   PLACE = 1,
   UPDATE = 2,
   DELETE = 3,
-  SVG = 4
+  SVG = 4,
 }
