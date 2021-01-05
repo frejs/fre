587b58011478e1861de84f186de43f7679993ef8
伊撒尔 <1533540012@qq.com>
2021-01-05 10:43:19 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-import { h, Fragment } from './h'
+import { h } from './h'
 import { render } from './reconciler'
 import { useState, useReducer, useEffect, useMemo, useCallback, useRef, useLayout } from './hooks'
 export * from './type'
 
 const options: Record<string,Function> = {}
-
 
 export {
   h,
@@ -20,6 +19,5 @@ export {
   useRef,
   useLayout,
   useLayout as useLayoutEffect,
-  Fragment,
   options,
 }
