1b3018864bfdc858c57c8e33f762f30488dc5929
132yse <1533540012@qq.com>
2021-01-30 18:38:40 +0800

    simplify state hook

--- demo/src/use-state.tsx
+++ demo/src/use-state.tsx
@@ -2,10 +2,12 @@ import { h, render, useEffect, useState } from '../../src/index'
 
 function App() {
   const [count, setCount] = useState(0)
-  // const [two, setTwo] = useState(0)
+  const [two, setTwo] = useState(0)
+  console.log(count,two)
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>{count}{count}</button>
+      <button onClick={() => setCount(count + 1)}>{count}</button>
+      <button onClick={() => setTwo(two + 1)}>{two}</button>
     </div>
   )
 }
