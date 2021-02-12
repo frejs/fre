c98af994bd8c187409a0a520d4025383071b5941
132yse <1533540012@qq.com>
2021-02-13 00:19:30 +0800

    fix fragment bug

--- demo/src/keys.tsx
+++ demo/src/keys.tsx
@@ -39,7 +39,7 @@ import { h, render, useEffect, useState } from '../../src/index'
 function App() {
   const [key, setKey] = useState([1, 2, 3])
   return <div>
-    <button onClick={() => setKey([3, 2, 1])}>x</button>,
+    <button onClick={() => setKey([3, 1, 2])}>x</button>,
     <ul>
       {key.map((i) => (
         // <Li i={i} key={i} />
@@ -48,6 +48,19 @@ function App() {
     </ul>
   </div>
 }
+
+// function App() {
+//   const [key, setKey] = useState([1, 2, 3])
+//   return <div>
+//     <button onClick={() => setKey([3, 2, 1])}>x</button>,
+//     <ul>
+//       {key.map((i) => (
+//         // <Li i={i} key={i} />
+//         <li key={i} >{i}</li>
+//       ))}
+//     </ul>
+//   </div>
+// }
 
 function Li(props) {
   return [<li>{props.i}</li>, <li>{props.i}</li>]
