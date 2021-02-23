21624d1255167f1b33c7f61743a69294d1c1a47c
Changhao Zhao （赵昌浩） <changhaozhao@trip.com>
2021-02-23 11:04:05 +0800

    update demo

--- demo/src/keys.tsx
+++ demo/src/keys.tsx
@@ -1,16 +1,16 @@
-import { h, render, useEffect, useState } from '../../src/index'
+import { h, render, useEffect, useState } from "../../src/index"
 
-function App() {
-  const [key, setKey] = useState([3,1,2])
-  return [
-    <button onClick={() => setKey([1,2,3])}>x</button>,
-    <ul>
-      {key.map((i) => (
-        <li key={i}>{i}</li>
-      ))}
-    </ul>,
-  ]
-}
+// function App() {
+//   const [key, setKey] = useState([3,1,2])
+//   return [
+//     <button onClick={() => setKey([1,2,3])}>x</button>,
+//     <ul>
+//       {key.map((i) => (
+//         <li key={i}>{i}</li>
+//       ))}
+//     </ul>,
+//   ]
+// }
 
 // function App() {
 //   const [key, setKey] = useState(['a', 'b', 'c'])
@@ -62,8 +62,23 @@ function App() {
 //   </div>
 // }
 
-function Li(props) {
-  return [<li>{props.i}</li>, <li>{props.i}</li>]
+function App() {
+  const [key, setKey] = useState([1, 2, 3])
+  return (
+    <div>
+      {key.map((i) => (
+        <Li i={i} key={i} />
+      ))}
+      <button onClick={() => setKey([3, 2, 1])}>x</button>
+    </div>
+  )
 }
 
-render(<App />, document.getElementById('root'))
+// function Li(props) {
+//   return [<li>{props.i}</li>, <li>{props.i}</li>]
+// }
+function Li(props) {
+  return <li>{props.i}</li>
+}
+
+render(<App />, document.getElementById("root"))
