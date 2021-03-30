7fe59a7fb70e5ddf2276b4f97cb92118d521cc54
Changhao Zhao （赵昌浩） <changhaozhao@trip.com>
2021-03-30 11:30:54 +0800

    emmm

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
-export { h, Fragment, h as createElement } from './h'
-export { render } from './reconciler'
-export { useState, useReducer, useEffect, useMemo, useCallback, useRef, useLayout, useLayout as useLayoutEffect } from './hooks'
-export { shouldYield } from './scheduler'
-export {lazy,Suspense} from './suspense'
-export * from './type'
-
-export const options: Record<string, Function> = {}
+export { h, Fragment, h as createElement } from "./h"
+export { render } from "./reconciler"
+export {
+  useState,
+  useReducer,
+  useEffect,
+  useMemo,
+  useCallback,
+  useRef,
+  useLayout,
+  useLayout as useLayoutEffect,
+} from "./hooks"
+export { shouldYield } from "./scheduler"
+export { lazy, Suspense } from "./suspense"
+export * from "./type"
