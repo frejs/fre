e3fd276c561b1e96eea6e40a0c80b586707e0686
Changhao Zhao （赵昌浩） <changhaozhao@trip.com>
2021-03-31 19:06:08 +0800

    deploy

--- docs/deno-deploy.ts
+++ docs/deno-deploy.ts
@@ -1,79 +1,83 @@
-function handleRequest(request) {
+async function handleRequest(request) {
   const { pathname } = new URL(request.url)
-  if (pathname === "/README.md") {
-    fetch("https://fre.js.org/README.md")
-      .then((res) => res.text())
-      .then((data) => {
-        return new Response(data, {
-          status: 200,
-          headers: {
-            server: "denosr",
-            "content-type": "text/plain",
-          },
-        })
-      })
-  }
 
+  if (pathname === "/") {
+    return index()
+  } else if(pathname[0] === '/'){
+    const data = await fetch(`https://fre.js.org${pathname}`)
+      .then((res) => res.text())
+      .then((data) => data)
+    return new Response(data, {
+      status: 200,
+      headers: {
+        server: "denosr",
+        "content-type": "text/plain",
+      },
+    })
+  }
+}
+
+function index() {
   return new Response(
     `<html lang="en">
-    <head>
-      <meta charset="UTF-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Fre doc</title>
-      <link rel="stylesheet" href="https://fre.js.org/docup.min.css">
-      <script src="https://fre.js.org/docup.fre.min.js"></script>
-      <style>
-          :root{
-              --navbar-bg: #3e2e98;
-              --navlink-hover-bg: #28127e;
-              --sidebar-bg: #f0f4ff;
-              --sidebar-menu-item-active-fg: #80092f;
-              --code-span-bg: #ffecfc;
-              --content-link-fg: rgb(189 30 104);
-              --code-block-bg: #302b38;
-          }
-          .content ul{
-              list-style-position: outside;
-              margin-left: 1.25rem;
-          }
-      </style>
-    </head>
-    <body>
-      <script>
-        docup.init({
-          title: "Fre",
-          highlightLanguages: ["nginx"],
-          props: {
-            langs: {},
-          },
-          navLinks: [
-            {
-              text: "Guide",
-              link: "#guide",
-            },
-            {
-              text: "API",
-              link: "#api",
-            },
-            {
-              text: "Resources",
-              link: "#resources",
-            },
-            {
-              text: "GitHub",
-              link: "https://github.com/yisar/fre",
-            },
-            {
-              text: "Donate",
-              link: "https://github.com/sponsors/yisar",
-            },
-          ],
-        })
-      </script>
-    </body>
-  </html>
-  `,
+        <head>
+          <meta charset="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>Fre doc</title>
+          <link rel="stylesheet" href="https://fre.js.org/docup.min.css">
+          <script src="https://fre.js.org/docup.fre.min.js"></script>
+          <style>
+              :root{
+                  --navbar-bg: #3e2e98;
+                  --navlink-hover-bg: #28127e;
+                  --sidebar-bg: #f0f4ff;
+                  --sidebar-menu-item-active-fg: #80092f;
+                  --code-span-bg: #ffecfc;
+                  --content-link-fg: rgb(189 30 104);
+                  --code-block-bg: #302b38;
+              }
+              .content ul{
+                  list-style-position: outside;
+                  margin-left: 1.25rem;
+              }
+          </style>
+        </head>
+        <body>
+          <script>
+            docup.init({
+              title: "Fre",
+              highlightLanguages: ["nginx"],
+              props: {
+                langs: {},
+              },
+              navLinks: [
+                {
+                  text: "Guide",
+                  link: "#guide",
+                },
+                {
+                  text: "API",
+                  link: "#api",
+                },
+                {
+                  text: "Resources",
+                  link: "#resources",
+                },
+                {
+                  text: "GitHub",
+                  link: "https://github.com/yisar/fre",
+                },
+                {
+                  text: "Donate",
+                  link: "https://github.com/sponsors/yisar",
+                },
+              ],
+            })
+          </script>
+        </body>
+      </html>
+      `,
     {
       headers: {
         "content-type": "text/html; charset=UTF-8",
