48d8f6135fd5583634c639e68ba96ad89bcb4e58
Changhao Zhao （赵昌浩） <changhaozhao@trip.com>
2021-04-01 19:56:59 +0800

    emmm

--- docs/deno-deploy.ts
+++ docs/deno-deploy.ts
@@ -1,25 +1,23 @@
 async function handleRequest(request) {
-  const { pathname } = new URL(request.url)
+  let { pathname } = new URL(request.url)
 
-  console.log(pathname)
+  pathname = pathname === "/" ? "/index.html" : pathname
 
-  if (pathname[0] === "/") {
-    const data = await fetch(
-      `https://raw.githubusercontent.com/yisar/fre/master/docs/${pathname}`
-    )
-      .then((res) => res.text())
-      .then((data) => data)
-    return new Response(data, {
-      status: 200,
-      headers: {
-        server: "denosr",
-        "content-type":
-          pathname === "/" || pathname === "/zh"
-            ? "text/html; charset=UTF-8"
-            : "text/plain",
-      },
-    })
-  }
+  const data = await fetch(
+    `https://raw.githubusercontent.com/yisar/fre/master/docs/${pathname}`
+  )
+    .then((res) => res.text())
+    .then((data) => data)
+  return new Response(data, {
+    status: 200,
+    headers: {
+      server: "denosr",
+      "content-type":
+        pathname === "/" || pathname === "/zh"
+          ? "text/html; charset=UTF-8"
+          : "text/plain",
+    },
+  })
 }
 
 addEventListener("fetch", (event: any) => {
