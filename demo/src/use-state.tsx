df3e894505a672fca0d0f270b2e089b930b1e0aa
Changhao Zhao （赵昌浩） <changhaozhao@trip.com>
2021-04-01 11:35:55 +0800

    update docs

--- demo/src/use-state.tsx
+++ demo/src/use-state.tsx
@@ -1,27 +1,28 @@
-import { h, render, useEffect, useState,Fragment } from "../../src/index"
+import { h, render, useEffect, useState, Fragment } from "../../src/index"
 
 function App() {
   const [count, setCount] = useState(0)
   const [two, setTwo] = useState(0)
+  useEffect(()=>{
+    setTimeout(()=>{
+      setCount(count+1)
+    },3000)
+  },[])
   return (
     <>
-      <Nil/>
-      <button
-        onClick={() =>
-          setCount((c) => {
-            return c + 1
-          })
-        }
-      >
-        {count}
+      <Nil count={count}/>
+      <button onClick={() => setCount((c) => c + 1)}>{count}</button>
+      <button onClick={() => setTwo(two + 1)} style="color:#2ef">
+        {two}
       </button>
-      <button onClick={() => setTwo(two + 1)} style="color:#2ef">{two}</button>
     </>
   )
 }
 
-function Nil(){
-  return <></>
+function Nil(props) {
+  console.log(props.count)
+  const [count, setCount] = useState(0)
+  return <button onClick={() => setCount((c) => c + 1)}>{count}</button>
 }
 
 render(<App />, document.body)
