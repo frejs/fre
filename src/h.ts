32b62b4358224c1d95b51f2b01cc9a472362dec4
132yse <1533540012@qq.com>
2021-04-24 18:15:33 +0800

    add recycleNode

--- src/h.ts
+++ src/h.ts
@@ -5,21 +5,28 @@ import { FreElement } from './type'
 export const h = (type, props, ...kids) => {
   props = props || {}
   kids = props.children || kids
-  let key = props.key || null, ref = props.ref || null
   while (kids.some(isArr)) kids = [].concat(...kids)
   kids = kids.map(kid => isStr(kid) ? createText(kid as string) : kid)
   if (kids.length) props.children = kids.length === 1 ? kids[0] : kids
   delete props.key
   delete props.ref
-  return createVnode(type, props, key, ref)
+  return createVnode(type, props, props.key, props.ref)
 }
 
-export const createVnode = (type, props, key, ref) => ({ type, props, key, ref })
+export const createVnode = (type, props, key = null, ref = null) => ({ type, props, key, ref })
 
 export const createText = (vnode: string) => ({ type: 'text', props: { nodeValue: vnode + '' } } as FreElement)
 
 export function Fragment(props) {
   return props.children
 }
+
+export const recycleNode = (node) =>
+  node.nodeType === 3
+    ? createText(node.nodeValue)
+    : createVnode(
+      node.nodeName.toLowerCase(),
+      { children: node.childNodes.map(recycleNode) }
+    )
 
 export const isArr = Array.isArray
