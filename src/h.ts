25224fe8c005c1c368016916d70b9245215262a1
Changhao Zhao （赵昌浩） <changhaozhao@trip.com>
2021-04-29 12:01:07 +0800

    fix h bug

--- src/h.ts
+++ src/h.ts
@@ -2,17 +2,19 @@ import { isStr } from './reconciler'
 import { FreElement } from './type'
 
 // for jsx2
-export const h = (type, props={}, ...kids) => {
+export const h = (type, props:any, ...kids) => {
+  props = props || {}
   kids = flat(props.children || kids)
   if (kids.length) props.children = kids.length === 1 ? kids[0] : kids
+  let key = props.key || null, ref = props.ref || null
   delete props.key
   delete props.ref
-  return createVnode(type, props, props.key, props.ref)
+  return createVnode(type, props, key, ref)
 }
 
-const flat = (arr) => [].concat(...arr.map(v => isArr(v) ? [].concat(flat(v)) : isStr(kid) ? createText(v) : v))
+const flat = (arr) => [].concat(...arr.map(v => isArr(v) ? [].concat(flat(v)) : isStr(v) ? createText(v) : v))
 
-export const createVnode = (type, props, key=null, ref=null) => ({ type, props, key, ref })
+export const createVnode = (type, props, key, ref) => ({ type, props, key, ref })
 
 export const createText = (vnode: any) => ({ type: 'text', props: { nodeValue: vnode + '' } } as FreElement)
 
