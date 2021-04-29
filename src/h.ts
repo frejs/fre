282bfe80c0f8bf7def1428f630574b967108ff78
伊撒尔 <1533540012@qq.com>
2021-04-29 11:47:49 +0800

    simplify h

--- src/h.ts
+++ src/h.ts
@@ -4,19 +4,18 @@ import { FreElement } from './type'
 // for jsx2
 export const h = (type, props, ...kids) => {
   props = props || {}
-  kids = props.children || kids
-  let key = props.key || null, ref = props.ref || null
-  while (kids.some(isArr)) kids = [].concat(...kids)
-  kids = kids.map(kid => isStr(kid) ? createText(kid as string) : kid)
+  kids = flat(props.children || kids)
   if (kids.length) props.children = kids.length === 1 ? kids[0] : kids
   delete props.key
   delete props.ref
-  return createVnode(type, props, key, ref)
+  return createVnode(type, props, props.key, props.ref)
 }
 
-export const createVnode = (type, props, key, ref) => ({ type, props, key, ref })
+const flat = (arr) => [].concat(...arr.map(v => isArr(v) ? [].concat(flat3(v)) : isStr(kid) ? createText(v) : v))
 
-export const createText = (vnode: string) => ({ type: 'text', props: { nodeValue: vnode + '' } } as FreElement)
+export const createVnode = (type, props, key=null, ref=null) => ({ type, props, key, ref })
+
+export const createText = (vnode: any) => ({ type: 'text', props: { nodeValue: vnode + '' } } as FreElement)
 
 export function Fragment(props) {
   return props.children
