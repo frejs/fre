44bc24278de69bd753fd80daf3940bada465908c
132 <changhaozhao@trip.com>
2021-05-07 17:44:49 +0800

    remove some

--- src/reconciler.ts
+++ src/reconciler.ts
@@ -323,4 +323,3 @@ export const getCurrentFiber = () => currentFiber || null
 export const isFn = (x: any): x is Function => typeof x === "function"
 export const isStr = (s: any): s is number | string =>
   typeof s === "number" || typeof s === "string"
-export const some = (v: any) => v != null && v !== false && v !== true
