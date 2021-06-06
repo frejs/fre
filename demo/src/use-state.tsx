f3ecc7166c200f751613e2d9ab790ebecf1c862b
132yse <1533540012@qq.com>
2021-06-06 12:37:27 +0800

    commit daily

--- demo/src/use-state.tsx
+++ demo/src/use-state.tsx
@@ -1,14 +1,21 @@
 import { render, useState } from "../../src/index"
 
 function App() {
-  console.log(123)
+  console.log('父组件')
   const [count, setCount] = useState(0)
   return (
-    <>
+    <div>
+      <B i={1}/>
+      <B i={2}/>
       <h1>{count}</h1>
       <button onClick={() => setCount(count + 1)}>+</button>
-    </>
+    </div>
   )
+}
+
+function B({i}){
+  console.log('子组件',i)
+  return 111
 }
 
 render(<App />, document.getElementById("app"))
