fc055b110ab86e92e921218dbc99573d7dc81750
132yse <1533540012@qq.com>
2021-06-26 07:50:31 +0800

    simplify

--- src/dom.ts
+++ src/dom.ts
@@ -3,30 +3,30 @@ import { isStr, LANE } from "./reconcile"
 
 export const updateElement = <P extends Attributes>(
   dom: DOM,
-  oldProps: P,
-  newProps: P
+  aProps: P,
+  bProps: P
 ) => {
-  for (let name in { ...oldProps, ...newProps }) {
-    let oldValue = oldProps[name]
-    let newValue = newProps[name]
+  for (let name in { ...aProps, ...bProps }) {
+    let a = aProps[name]
+    let b = bProps[name]
 
-    if (oldValue === newValue || name === "children") {
-    } else if (name === "style" && !isStr(newValue)) {
-      for (const k in { ...oldValue, ...newValue }) {
-        if (!(oldValue && newValue && oldValue[k] === newValue[k])) {
-          ;(dom as any)[name][k] = newValue?.[k] || ""
+    if (a === b || name === "children") {
+    } else if (name === "style" && !isStr(b)) {
+      for (const k in { ...a, ...b }) {
+        if (!(a && b && a[k] === b[k])) {
+          ; (dom as any)[name][k] = b?.[k] || ""
         }
       }
     } else if (name[0] === "o" && name[1] === "n") {
       name = name.slice(2).toLowerCase() as Extract<keyof P, string>
-      if (oldValue) dom.removeEventListener(name, oldValue)
-      dom.addEventListener(name, newValue)
+      if (a) dom.removeEventListener(name, a)
+      dom.addEventListener(name, b)
     } else if (name in dom && !(dom instanceof SVGElement)) {
-      ;(dom as any)[name] = newValue||''
-    } else if (newValue == null || newValue === false) {
+      ; (dom as any)[name] = b || ''
+    } else if (b == null || b === false) {
       dom.removeAttribute(name)
     } else {
-      dom.setAttribute(name, newValue)
+      dom.setAttribute(name, b)
     }
   }
 }
@@ -36,11 +36,11 @@ export const createElement = <P = Attributes>(fiber: IFiber) => {
     fiber.type === ""
       ? document.createTextNode("")
       : fiber.lane & LANE.SVG
-      ? document.createElementNS(
+        ? document.createElementNS(
           "http://www.w3.org/2000/svg",
           fiber.type as string
         )
-      : document.createElement(fiber.type as string)
+        : document.createElement(fiber.type as string)
   updateElement(dom as DOM, {} as P, fiber.props as P)
   return dom
 }
