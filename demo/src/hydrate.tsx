68640cbc78af0e42536b98ec563411797f60d547
changhaozhao <changhaozhao@trip.com>
2022-01-25 18:50:14 +0800

    support ssr

--- demo/src/hydrate.tsx
+++ demo/src/hydrate.tsx
@@ -13,45 +13,6 @@ function App() {
   )
 }
 
-function walker(node) {
-  return document.createTreeWalker(node, NodeFilter.SHOW_ELEMENT, {
-    acceptNode: () => NodeFilter.FILTER_ACCEPT,
-  })
-}
-
-function morph(src, tar) {
-  const sw = walker(src)
-  const tw = walker(tar)
-  const walk = fn => sw.nextNode() && tw.nextNode() && fn() && walk(fn)
-
-  walk(() => {
-    const c = sw.currentNode
-    const t = tw.currentNode
-
-    if (c.tagName === t.tagName) {
-      // TODO more things
-
-      if (document.activeElement === t) {
-        nextTick(() => c.focus())
-      }
-    }
-    return true
-  })
-}
-
-function hydrate(vnode, node, config = {}) {
-  let hydrated = false
-  const clone = node.cloneNode(false)
-  config.done = () => {
-    morph(clone, node)
-    if (!hydrated) {
-      node.parentNode.replaceChild(clone, node)
-    }
-    hydrated = true
-  }
-  render(vnode, clone, config)
-}
-
-hydrate(<App />, document.getElementById('app'))
+render(<App />, document.getElementById('app'))
 
 document.querySelector('#focus').focus()
