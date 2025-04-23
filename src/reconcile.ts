678eefb1f621b8bc4f401c0b59df2653d0bacd01
yisar <1533540012@qq.com>
2025-04-23 13:31:57 +0800

    keep  order

--- src/reconcile.ts
+++ src/reconcile.ts
@@ -250,7 +250,9 @@ const diff = (aCh, bCh) => {
       }
     }
   }
-  actions.concat(temp)
+  for (let i = temp.length - 1; i >= 0; i--) {
+    actions.push(temp[i])
+  }
   return actions
 }
 export const getCurrentFiber = () => currentFiber || null
