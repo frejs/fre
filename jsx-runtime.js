05d20e5b3300479f832dda39845373eb75b256e0
RosApr <287134285@qq.com>
2025-09-19 16:24:01 +0800

    fix jsx runtime bug

--- jsx-runtime.js
+++ jsx-runtime.js
@@ -1,1 +1,8 @@
-export { h as jsx, h as jsxs, Fragment } from "./dist/fre.js"
+import { h, Fragment } from './dist/fre.js'
+export { Fragment }
+export const jsx = function(type, props, key) {
+    return h(type, {...props, key});
+}
+export const jsxs = function(type, props, key) {
+    return h(type, {...props, key});
+}
